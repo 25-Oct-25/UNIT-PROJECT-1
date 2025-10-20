48205aed8a3784ab7c2ef872c6ee0b7cf346152d
Lama <lamamalharbi01@gmail.com>
2025-10-20 18:31:58 +0300

    Initial step for the new application idea 'Trading Analyzer'

---
+++ TradingAnalyzerApplication/data_fetcher.py
@@ -0,0 +1,43 @@
+import pandas as pd
+from nba_api.stats.endpoints import leaguedashplayerstats
+
+class DataFetcher:
+    '''Handles fetching row NBA player stats and initial data preparation.'''
+
+    def __init__(self, season= '2024-25', min_gp=20, categories= []):
+
+        self.season= season
+        self.min_gp= min_gp
+        self.categories= categories
+
+    def fetch_and_prepare(self):
+        '''Fetches player stats from the NBA API and filters the DataFrame.'''
+        print(f"fetching stats for season {self.season}...")
+
+        try:
+            data = leaguedashplayerstats.LeagueDashPlayerStats(
+                season=self.season,
+                per_mode_simple = 'PerGame'
+            )
+
+            df= data.get_data_frames()[0]
+
+        except Exception as e:
+            print(f"Error occured while fetching data from NBA API. Check connections: {e}")
+            return None
+        
+        df_filtered = df[df['GP'] >= self.min_gp].copy()
+
+
+        selected_cols= ['PLAYER_NAME', 'GP', 'MIN'] + self.categories
+        df_stats= df_filtered[selected_cols].copy()
+
+        df_stats['FGM'] = df_filtered['FGM']
+        df_stats['FGA'] = df_filtered['FGA']
+        df_stats['FTM'] = df_filtered['FTM']
+        df_stats['FTA'] = df_filtered['FTA']
+
+
+        print(f"Data fetched and filtered successfuly. Found {len(df_stats)} qualified players.")
+        return df_stats
+        
