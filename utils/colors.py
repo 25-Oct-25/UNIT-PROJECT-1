ff91a80b843ca38035b473835b65ab007297c88a
Ghala <ghalanmasi@icloud.com>
2025-10-20 11:59:49 +0300

    still working on the project

---
+++ utils/colors.py
@@ -0,0 +1,11 @@
+from colorama import Fore, Style, init
+
+init(autoreset=True)
+
+RED = Fore.RED
+GREEN = Fore.GREEN
+BLUE = Fore.BLUE
+YELLOW = Fore.YELLOW
+CYAN = Fore.CYAN
+PURPLE = Fore.MAGENTA
+RESET = Style.RESET_ALL
