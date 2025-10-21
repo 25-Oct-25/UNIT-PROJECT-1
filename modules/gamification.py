8cf04e1ee13a1118e0725501877d4d455979e677
Ziyad <zsyz8335@gmai.com>
2025-10-21 19:09:04 +0300

    delete unimportant files

--- modules/gamification.py
+++
@@ -1,8 +0,0 @@
-user_points = {}
-
-def award_points(email, points):
-    user_points[email] = user_points.get(email, 0) + points
-
-def leaderboard():
-    for u,p in sorted(user_points.items(), key=lambda x:x[1], reverse=True):
-        print(f"{u}: {p} pts")
--- modules/theme_suggestions.py
+++
@@ -1,5 +0,0 @@
-import random
-
-def suggest_theme(keyword):
-    themes = ["Neon Night", "Vintage Chic", "Minimalist", "Tropical Sunset", "Futuristic"]
-    return random.choice(themes) + f" ({keyword})"
